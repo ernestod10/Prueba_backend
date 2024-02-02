f806358b746f04b37f919b0859d65b6358402c29
Ernesto <48799528+ernestod10@users.noreply.github.com>
2024-02-02 00:31:18 -0400

    intento de prueba unitaria
    mocha me esta dando un error

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,25 +1,27 @@
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../src/index';
 
 chai.use(chaiHttp);
-const expect = chai.expect;
 
-describe('post /singup', () => {
-    it('should return 200', (done) => {
-        chai.request(app)
-        .post('/signup')
-        .send({
-            username: 'test',
-            email: 'test@test.com',
-            password: 'test1234',
-            password2: 'test1234'
-        })
-        .end((err, res) => {
-            expect(res).to.have.status(200);
-            done();
+describe('User', () => {
+    describe('POST /signup', () => {
+        it('should create a new user', (done) => {
+            chai.request(app)
+                .post('/signup')
+                .send({
+                    username: 'test',
+                    email: 'test@test.com',
+                    password: 'test1234',
+                    password2: 'test1234'
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('username', 'test');
+                    expect(res.body).to.have.property('email', 'test@test.com');
+                    // Add more assertions as needed
+                    done();
+                });
         });
-    }
-    );
-}
-);
+    });
+});
